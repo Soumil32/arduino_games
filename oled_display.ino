86bdc81b674914be9243fda5105713ddf3d3bd15
Soumil <soumil009@outlook.com>
2023-09-26 18:43:04 +0100

    swithced to platform io

--- oled_display.ino
+++ oled_display.ino
@@ -6,7 +6,7 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define BUTTON_PIN 6
 #define PLAYER_JUMP_POWER 35
-#define FRAMES_TO_JUMP 15
+#define MS_TO_JUMP 100
 #define OBSTACLE_SPEED 10 // this is in pixels per second
 #define OBSTACLE_SPAWN_RATE_MAX 2000 // this is the max amount of time to wait in milliseconds
 #define OBSTACLE_SPAWN_RATE_MIN 1200 // this is the min amount of time to wait in milliseconds
@@ -67,61 +67,11 @@ bool check_if_out_of_bounds(int x, int y, int width, int height) {
   }
 }
 
-// get acceleration for each frame when there are 5 frames to jump
-double get_acceleration_5(int frame, int jump_power) {
-  double frame1 = jump_power / 3;
-  if (frame == 1) {
-    return frame1;
-  }
-  double frame3 = jump_power / 5;
-  if (frame == 3) {
-    return frame3;
-  }
-  if (frame == 2) {
-    double frame2 = (frame1 + frame3) / 2;
-    // if frame 2 is not an integer, then it will be rounded up
-    // if frame 2 is a integer, then add 1 to it
-    if (frame2 - (int)frame2 != 0) {
-      return ceil(frame2);
-    } else {
-      return frame2 + 1;
-    }
-  }
-  double frame5 = 1;
-  if (frame == 5) {
-    return frame5;
-  }
-  if (frame == 4) {
-    double frame4 = (frame3 + frame5) / 2;
-    if (frame4 - (int)frame4 != 0) {
-      return ceil(frame4);
-    } else {
-      return frame4 + 1;
-    }
-  }
-}
-
 // get acceleration for each frame when there are 10 frames to jump
-double get_acceleration_10(int frame, int jump_power) {
-  const double divide_by_double = 4;
-  const int divide_by_int = 4;
-  if (frame / divide_by_int == 0) {
-    return get_acceleration_5(1, jump_power) / divide_by_double;
-  } else if (frame / divide_by_int == 1) {
-    return get_acceleration_5(2, jump_power) / divide_by_double;
-  } else if (frame / divide_by_int == 2) {
-    return get_acceleration_5(3, jump_power) / divide_by_double;
-  } else if (frame / divide_by_int == 3) {
-    return get_acceleration_5(4, jump_power) / divide_by_double;
-  } else if (frame / divide_by_int == 4) {
-    return get_acceleration_5(5, jump_power) / divide_by_double;
-  } else if (frame / divide_by_int == 5) { 
-    return 0;
-  } else {
-    Serial.print("Error: frame is out of bounds: ");
-    Serial.println(frame);
-    return 0;
-  }
+double get_acceleration(int time_passed, int total_time, float delta_time) {
+  // in the first 5th of the time to jump, the player will accelerate by 1/3 of the total acceleration over the first 5th of the time to jump
+  // by the end of the first 5th of the time to jump, the player will have accelerated by 1/3  in total of the total acceleration
+  // after that until the 
 }
 
 /// @brief plays the game 
@@ -135,7 +85,7 @@ void gameLoop(float deltaTime) {
   static double player_acceleration_added = 0;
   static double player_y = SCREEN_HEIGHT - height - 5;
   static int player_state = 0; // 0 = idle, 1 = jumping, 2 = falling
-  static int jump_frame = 1;
+  static int time_since_jump = 1; // this is in milliseconds
   static int time_since_last_spawn = 0; // this is in milliseconds
   static bool game_over = false;
   static float score = 0;
@@ -145,29 +95,28 @@ void gameLoop(float deltaTime) {
     player_state = 1;
   }
   if (player_state == 1) {
-    double player_acceleration_this_frame = get_acceleration_10(jump_frame, PLAYER_JUMP_POWER);
+    double player_acceleration_this_frame = get_acceleration(time_since_jump, PLAYER_JUMP_POWER, deltaTime);
     player_y -= player_acceleration_this_frame;
     player_acceleration_added += player_acceleration_this_frame;
-    jump_frame++;
+    time_since_jump++;
     if (player_acceleration_added >= PLAYER_JUMP_POWER) {
       player_state = 2;
-    } else if (jump_frame > FRAMES_TO_JUMP) {
+    } else if (time_since_jump > MS_TO_JUMP) {
       player_state = 2;
     }
   } else if (player_state == 2) {
-    double player_acceleration_this_frame = get_acceleration_10(jump_frame, PLAYER_JUMP_POWER);
+    double player_acceleration_this_frame = get_acceleration(time_since_jump, PLAYER_JUMP_POWER, deltaTime);
     player_y += player_acceleration_this_frame;
     player_acceleration_added -= player_acceleration_this_frame;
-    jump_frame--;
+    time_since_jump -= deltaTime * 1000;
     if (player_acceleration_added <= 0) {
       player_state = 0;
-      jump_frame = 1;
+      time_since_jump = 0;
     }
   }
   
   int timeToWait = random(OBSTACLE_SPAWN_RATE_MIN, OBSTACLE_SPAWN_RATE_MAX);
   if (time_since_last_spawn >= timeToWait) {
-    Serial.println(timeToWait);
     // generate a random number which will decide if an obstacle will be spawned
     int random_number = random(0, 3);
     if (random_number == 1) {
@@ -232,7 +181,7 @@ void gameLoop(float deltaTime) {
     player_acceleration_added = 0;
     player_y = SCREEN_HEIGHT - height - 5;
     player_state = 0;
-    jump_frame = 1;
+    time_since_jump = 0;
     time_since_last_spawn = 0;
     game_over = false;
     score = 0;
